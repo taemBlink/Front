f520c689d7c534620a1e60a03477654a725b7bba
choijihye1017 <chlwi6752@naver.com>
2023-05-27 20:17:04 +0900

    레이아웃-바디파일 원상복귀

--- src/Components/Layout/Bodies/Body.jsx
+++ src/Components/Layout/Bodies/Body.jsx
@@ -1,43 +1,5 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-
 function Body({ children }) {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    // 백엔드에서 글목록 데이터를 가져오는 비동기 함수 호출
-    fetchPosts();
-  }, []);
-
-  const fetchPosts = async () => {
-    try {
-      const response = await fetch("백엔드 API URL");
-      const data = await response.json();
-      setPosts(data); // 가져온 데이터를 상태에 저장
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  };
-
-  return (
-    <div className="content">
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-          <StImgBox imageUrl={post.imageURL}></StImgBox>
-        </div>
-      ))}
-      {children}
-    </div>
-  );
+  return <div className="content">{children}</div>;
 }
 
 export default Body;
-const StImgBox = styled.div`
-  background-image: url(${(props) => props.imageUrl});
-  background-size: cover;
-  background-position: center;
-  width: 100%;
-  height: 80%;
-`;
